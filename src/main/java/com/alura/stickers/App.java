4727bae7d8c2359f6f18342f5cc085356a4bd8f7
peter <peterleite.dev@gmail.com>
2023-04-03 23:05:03 -0300

    Create Http conection with TopMovies json

--- src/main/java/com/alura/stickers/App.java
+++ src/main/java/com/alura/stickers/App.java
@@ -1,7 +1,28 @@
 package com.alura.stickers;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello, World");
+
+        //Create HTTP connection
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        var adress = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(adress).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
+
+        //Parse data (title, poster, rating)
+
+        //display data
     }
 }
