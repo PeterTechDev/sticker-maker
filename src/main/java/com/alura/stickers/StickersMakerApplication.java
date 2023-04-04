fa90a80f64b0d1520e96277170e54b343a5770f6
peter <peterleite.dev@gmail.com>
2023-04-03 22:40:18 -0300

    initial commmit

---
+++ src/main/java/com/alura/stickers/StickersMakerApplication.java
@@ -0,0 +1,13 @@
+package com.alura.stickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickersMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickersMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/stickers/StickersMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.stickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickersMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
